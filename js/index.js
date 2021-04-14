2dad9c738f2e7bf4364d7e8ba40f4f06924d7097
mreadv <mariareadv@gmail.com>
2021-04-14 18:25:14 -0400

    reverted back to previous js

--- js/index.js
+++ js/index.js
@@ -2,8 +2,7 @@ const randomFolks = document.querySelector(".random-peeps");
 const getData = async function () {
     const usersRequest = await fetch ("https://randomuser.me/api?results=5");
     const data = await usersRequest.json();
-    const usersResults = data.results;
+    
 };
 
 getData();
-console.log(getData);
